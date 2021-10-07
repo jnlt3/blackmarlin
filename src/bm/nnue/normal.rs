0cf1d91c0747e724291651f3fbc97054127383b1
doruk <dsekercioglu2003@gmail.com>
2021-10-07 22:35:47 +0300

    cargo fmt

--- src/bm/nnue/normal.rs
+++ src/bm/nnue/normal.rs
@@ -75,7 +75,9 @@ impl<const INPUT: usize, const OUTPUT: usize> Dense<INPUT, OUTPUT> {
     #[inline]
     pub fn ff_sym(&self, w_inputs: &[i8; INPUT], b_inputs: &[i8; INPUT]) -> [i16; OUTPUT] {
         let mut out = [0_i16; OUTPUT];
-        for ((&w_input, &b_input), weights) in w_inputs.iter().zip(b_inputs.iter()).zip(&*self.weights.0) {
+        for ((&w_input, &b_input), weights) in
+            w_inputs.iter().zip(b_inputs.iter()).zip(&*self.weights.0)
+        {
             for (out, &weight) in out.iter_mut().zip(weights) {
                 *out += weight as i16 * (w_input as i16 - b_input as i16) / 2;
             }
