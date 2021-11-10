9c83f1802458e0b0eef0cf130a6046bfcaa56ca3
doruk <dsekercioglu2003@gmail.com>
2021-11-10 20:24:01 +0300

    Simplify

--- src/bm/nnue/normal.rs
+++ src/bm/nnue/normal.rs
@@ -4,28 +4,22 @@ const MIN: i16 = 0;
 const MAX: i16 = SCALE;
 
 #[derive(Debug, Clone)]
-pub struct Weights<'a, const INPUT: usize, const OUTPUT: usize>(&'a [[i8; OUTPUT]; INPUT]);
-
-#[derive(Debug, Clone)]
-pub struct WeightsI32<'a, const INPUT: usize, const OUTPUT: usize>(&'a [[i32; OUTPUT]; INPUT]);
-
-#[derive(Debug, Clone)]
 pub struct Psqt<'a, const INPUT: usize, const OUTPUT: usize> {
-    weights: WeightsI32<'a, INPUT, OUTPUT>,
+    weights: &'a [[i32; OUTPUT]; INPUT],
     out: [i32; OUTPUT],
 }
 
 impl<'a, const INPUT: usize, const OUTPUT: usize> Psqt<'a, INPUT, OUTPUT> {
     pub fn new(weights: &'a [[i32; OUTPUT]; INPUT]) -> Self {
         Self {
-            weights: WeightsI32(weights),
+            weights,
             out: [0_i32; OUTPUT],
         }
     }
 
     #[inline]
     pub fn incr_ff<const CHANGE: i32>(&mut self, index: usize) {
-        for (out, &weight) in self.out.iter_mut().zip(&self.weights.0[index]) {
+        for (out, &weight) in self.out.iter_mut().zip(&self.weights[index]) {
             *out += weight * CHANGE;
         }
     }
@@ -37,21 +31,18 @@ impl<'a, const INPUT: usize, const OUTPUT: usize> Psqt<'a, INPUT, OUTPUT> {
 
 #[derive(Debug, Clone)]
 pub struct Incremental<'a, const INPUT: usize, const OUTPUT: usize> {
-    weights: Weights<'a, INPUT, OUTPUT>,
+    weights: &'a [[i8; OUTPUT]; INPUT],
     out: [i16; OUTPUT],
 }
 
 impl<'a, const INPUT: usize, const OUTPUT: usize> Incremental<'a, INPUT, OUTPUT> {
     pub fn new(weights: &'a [[i8; OUTPUT]; INPUT], bias: [i16; OUTPUT]) -> Self {
-        Self {
-            weights: Weights(weights),
-            out: bias,
-        }
+        Self { weights, out: bias }
     }
 
     #[inline]
     pub fn incr_ff<const CHANGE: i16>(&mut self, index: usize) {
-        for (out, &weight) in self.out.iter_mut().zip(&self.weights.0[index]) {
+        for (out, &weight) in self.out.iter_mut().zip(&self.weights[index]) {
             *out += weight as i16 * CHANGE;
         }
     }
@@ -63,14 +54,14 @@ impl<'a, const INPUT: usize, const OUTPUT: usize> Incremental<'a, INPUT, OUTPUT>
 
 #[derive(Debug, Clone)]
 pub struct Dense<'a, const INPUT: usize, const OUTPUT: usize> {
-    weights: Weights<'a, INPUT, OUTPUT>,
+    weights: &'a [[i8; OUTPUT]; INPUT],
     bias: [i32; OUTPUT],
 }
 
 impl<'a, const INPUT: usize, const OUTPUT: usize> Dense<'a, INPUT, OUTPUT> {
     pub fn new(weights: &'a [[i8; OUTPUT]; INPUT], bias: [i16; OUTPUT]) -> Self {
         Self {
-            weights: Weights(weights),
+            weights,
             bias: i16_to_i32(bias),
         }
     }
@@ -84,7 +75,7 @@ impl<'a, const INPUT: usize, const OUTPUT: usize> Dense<'a, INPUT, OUTPUT> {
     ) -> [i32; OUTPUT] {
         let mut out = self.bias;
         for ((&w_input, &b_input), weights) in
-            w_inputs.iter().zip(b_inputs.iter()).zip(&*self.weights.0)
+            w_inputs.iter().zip(b_inputs.iter()).zip(&*self.weights)
         {
             for (out, &weight) in out[bucket..bucket + 1]
                 .iter_mut()
