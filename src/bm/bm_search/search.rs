b99f5b1a8a200ab06eb328ec3448a3fa1af16386
doruk <dsekercioglu2003@gmail.com>
2021-11-30 15:03:25 +0300

    Don't drop into qsearch

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -429,7 +429,7 @@ pub fn search<Search: SearchType>(
                 if !is_quiet {
                     reduction -= 1;
                 }
-                reduction = reduction.min(depth as i16 - 1).max(1);
+                reduction = reduction.min(depth as i16 - 2).max(1);
             }
 
             let lmr_depth = (depth as i16 - reduction).max(1) as u32;
