4cc61e6379b328d47b2d227d94a25c3f378762c4
doruk <dsekercioglu2003@gmail.com>
2021-12-06 18:15:03 +0300

    follow fail_tm TM

--- src/bm/bm_runner/time.rs
+++ src/bm/bm_runner/time.rs
@@ -5,7 +5,7 @@ use std::sync::atomic::{AtomicBool, AtomicI16, AtomicU32, Ordering};
 use std::sync::Mutex;
 use std::time::{Duration, Instant};
 
-const EXPECTED_MOVES: u32 = 50;
+const EXPECTED_MOVES: u32 = 40;
 
 const TIME_DEFAULT: Duration = Duration::from_secs(0);
 const INC_DEFAULT: Duration = Duration::from_secs(0);
@@ -86,9 +86,9 @@ impl TimeManager {
         if eval_diff < 0.0 {
             eval_diff *= 1.5;
         };
-        eval_diff = eval_diff.min(1.0).abs();
+        eval_diff = eval_diff.abs().min(1.0);
 
-        time *= 1.025_f32.powf(eval_diff);
+        time *= 1.05_f32.powf(eval_diff);
 
         let time = time.min(self.max_duration.load(Ordering::SeqCst) as f32 * 1000.0);
         self.normal_duration
