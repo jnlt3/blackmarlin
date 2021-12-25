3fa2e436f989d7c7557fbcbba2cac620f832f9a6
doruk <dsekercioglu2003@gmail.com>
2021-12-25 20:58:51 +0300

    less LMR if gives check

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -451,6 +451,9 @@ pub fn search<Search: SearchType>(
                 if improving {
                     reduction -= 1;
                 }
+                if gives_check {
+                    reduction -= 1;
+                }
                 reduction = reduction.min(depth as i16 - 1).max(0);
             }
 
