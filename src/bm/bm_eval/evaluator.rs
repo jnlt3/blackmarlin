d597ba5e13f7092edd021b9f1714816322a7cfff
doruk <dsekercioglu2003@gmail.com>
2021-12-27 15:51:56 +0300

    Scale by 0.75

--- src/bm/bm_eval/evaluator.rs
+++ src/bm/bm_eval/evaluator.rs
@@ -362,9 +362,10 @@ impl StdEvaluator {
         }
         let piece_cnt = pieces.popcnt();
         let queen_cnt = queens.popcnt();
-        0.8 + pawn_cnt as f32 * (1.0 / 48.0)
+        (0.8 + pawn_cnt as f32 * (1.0 / 48.0)
             + piece_cnt as f32 * (1.0 / 24.0)
-            + queen_cnt as f32 * (1.0 / 12.0)
+            + queen_cnt as f32 * (1.0 / 12.0))
+            * 0.75
     }
 
     /**
