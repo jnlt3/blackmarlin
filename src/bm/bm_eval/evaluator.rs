0605638bc35be1660acb2122a42bb3a87db0ab3e
doruk <dsekercioglu2003@gmail.com>
2021-12-31 08:56:39 +0300

    Lower base scale, smooth formula

--- src/bm/bm_eval/evaluator.rs
+++ src/bm/bm_eval/evaluator.rs
@@ -355,13 +355,10 @@ impl StdEvaluator {
         let pawns = board.pieces(Piece::Pawn);
         let pieces = board.occupied() & !pawns;
         let queens = board.pieces(Piece::Queen);
-        let mut pawn_cnt = pawns.popcnt() as i16;
-        if pawn_cnt == 0 {
-            pawn_cnt = -16;
-        }
+        let pawn_cnt = pawns.popcnt() as i16;
         let piece_cnt = pieces.popcnt();
         let queen_cnt = queens.popcnt();
-        0.8 + pawn_cnt as f32 * (1.0 / 48.0)
+        0.5 + pawn_cnt as f32 * (1.0 / 48.0)
             + piece_cnt as f32 * (1.0 / 24.0)
             + queen_cnt as f32 * (1.0 / 12.0)
     }
