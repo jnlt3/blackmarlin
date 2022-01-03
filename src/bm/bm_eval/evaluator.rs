d118b6ae7f5aeebfdc056b47712d46df6ba13854
doruk <dsekercioglu2003@gmail.com>
2022-01-03 10:54:07 +0300

    Old eval

--- src/bm/bm_eval/evaluator.rs
+++ src/bm/bm_eval/evaluator.rs
@@ -349,6 +349,22 @@ impl StdEvaluator {
         }
     }
 
+    #[cfg(feature = "nnue")]
+    fn eval_scale(board: &Board) -> f32 {
+        let pawns = board.pieces(Piece::Pawn);
+        let pieces = board.occupied() & !pawns;
+        let queens = board.pieces(Piece::Queen);
+        let mut pawn_cnt = pawns.popcnt() as i16;
+        if pawn_cnt == 0 {
+            pawn_cnt = -16;
+        }
+        let piece_cnt = pieces.popcnt();
+        let queen_cnt = queens.popcnt();
+        0.8 + pawn_cnt as f32 * (1.0 / 48.0)
+            + piece_cnt as f32 * (1.0 / 24.0)
+            + queen_cnt as f32 * (1.0 / 12.0)
+    }
+
     /**
     Doesn't handle checkmates or stalemates
      */
@@ -362,8 +378,10 @@ impl StdEvaluator {
         };
         #[cfg(feature = "nnue")]
         {
+            let scale = Self::eval_scale(board);
             let nnue_out = self.nnue.feed_forward(board, 0);
-            Evaluation::new(nnue_out * turn + NNUE_TEMPO)
+            let scaled = (nnue_out as f32 * scale) as i16;
+            Evaluation::new(scaled * turn + NNUE_TEMPO)
         }
         #[cfg(not(feature = "nnue"))]
         {
