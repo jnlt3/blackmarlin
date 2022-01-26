a156e72fdbf2a8b5e08d7e97d10a6f1a7713d16d
doruk <dsekercioglu2003@gmail.com>
2022-01-27 01:13:25 +0300

    Prune captures less with history

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -386,8 +386,12 @@ pub fn search<Search: SearchType>(
             if it has very low history
             */
             let do_hp = !Search::PV && depth <= 8 && eval <= alpha;
-
-            if do_hp && (h_score as i32) < (-h_table::MAX_VALUE * ((depth * depth) as i32) / 64) {
+            let h_depth = if is_capture {
+                depth + 1
+            } else {
+                depth
+            };
+            if do_hp && (h_score as i32) < (-h_table::MAX_VALUE * ((h_depth * h_depth) as i32) / 64) {
                 continue;
             }
 
@@ -405,7 +409,7 @@ pub fn search<Search: SearchType>(
                 move_gen.set_skip_quiets(true);
                 continue;
             }
-            
+
             /*
             In non-PV nodes If a move evaluated by SEE isn't good enough to beat alpha - a static margin
             we assume it's safe to prune this move
