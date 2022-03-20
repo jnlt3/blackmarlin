bb70fb781fe098ee27555cfa0d3f37f729901757
doruk <dsekercioglu2003@gmail.com>
2022-03-21 00:02:28 +0300

    Simplify formula

--- src/bm/bm_runner/time.rs
+++ src/bm/bm_runner/time.rs
@@ -1,7 +1,7 @@
 use crate::bm::bm_util::eval::Evaluation;
 use cozy_chess::{Board, Move};
 use std::fmt::Debug;
-use std::sync::atomic::{AtomicBool, AtomicI16, AtomicU32, Ordering, AtomicU64};
+use std::sync::atomic::{AtomicBool, AtomicI16, AtomicU32, AtomicU64, Ordering};
 use std::sync::Mutex;
 use std::time::{Duration, Instant};
 
@@ -111,9 +111,11 @@ impl TimeManager {
 
         time *= 1.05_f32.powf(eval_diff.min(1.0));
 
-        let move_change_factor = 1.05_f32
-            .powf(MOVE_CHANGE_MARGIN as f32 - move_change_depth as f32)
-            .max(0.4);
+        let move_change_factor = if move_change_depth > MOVE_CHANGE_MARGIN {
+            0.4
+        } else {
+            1.2
+        };
 
         let time = time.min(self.max_duration.load(Ordering::SeqCst) as f32 * 1000.0);
         self.normal_duration
