1d5649f235c4a3008c19c2ec8013eb7d37f6d235
doruk <dsekercioglu2003@gmail.com>
2022-03-22 23:45:24 +0300

    Punish extensions less

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -318,7 +318,7 @@ pub fn search<Search: SearchType>(
             if moves_seen == 0
                 && entry.table_move() == make_move
                 && ply != 0
-                && depth * depth >= 49 + local_context.extensions()
+                && depth * depth >= 49 + local_context.extensions() / 4
                 && !entry.score().is_mate()
                 && entry.depth() >= depth - 2
                 && (entry.entry_type() == EntryType::LowerBound
