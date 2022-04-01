9b290381576ae2eab842ef5eda49482a8491d1d7
doruk <dsekercioglu2003@gmail.com>
2022-04-01 21:03:30 +0300

    Improving in NMP

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -52,10 +52,16 @@ const fn rev_fp(depth: u32, improving: bool) -> i16 {
 }
 
 #[inline]
-fn do_nmp<Search: SearchType>(board: &Board, depth: u32, eval: i16, beta: i16) -> bool {
+fn do_nmp<Search: SearchType>(
+    board: &Board,
+    depth: u32,
+    eval: i16,
+    beta: i16,
+    improving: bool,
+) -> bool {
     Search::NM
         && depth > 4
-        && eval >= beta
+        && eval + improving as i16 * 50 >= beta
         && (board.pieces(Piece::Pawn) | board.pieces(Piece::King)) != board.occupied()
 }
 
@@ -212,7 +218,9 @@ pub fn search<Search: SearchType>(
         This is seen as the major threat in the current position and can be used in
         move ordering for the next ply
         */
-        if do_nmp::<Search>(pos.board(), depth, eval.raw(), beta.raw()) && pos.null_move() {
+        if do_nmp::<Search>(pos.board(), depth, eval.raw(), beta.raw(), improving)
+            && pos.null_move()
+        {
             local_context.search_stack_mut()[ply as usize].move_played = None;
             let zw = beta >> Next;
             let search_score = search::<NoNm>(
