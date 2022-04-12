814859ac84054962bde25fb90396fc7f158ff2a1
doruk <dsekercioglu2003@gmail.com>
2022-04-12 22:32:14 +0200

    Don't prune queen promotions

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -292,6 +292,9 @@ pub fn search<Search: SearchType>(
             .board()
             .colors(!pos.board().side_to_move())
             .has(make_move.to);
+        let is_q_promo = make_move.promotion == Some(Piece::Queen);
+
+        let is_noisy = is_capture || is_q_promo;
 
         let h_score = if is_capture {
             local_context.get_ch_table().get(
@@ -361,7 +364,7 @@ pub fn search<Search: SearchType>(
         In non-PV nodes If a move isn't good enough to beat alpha - a static margin
         we assume it's safe to prune this move
         */
-        let do_fp = !Search::PV && moves_seen > 0 && !is_capture && depth <= 7;
+        let do_fp = !Search::PV && moves_seen > 0 && !is_noisy && depth <= 7;
 
         if do_fp && eval + fp(depth) <= alpha {
             move_gen.set_skip_quiets(true);
@@ -372,7 +375,7 @@ pub fn search<Search: SearchType>(
         If a move is placed late in move ordering, we can safely prune it based on a depth related margin
         */
         if !move_gen.skip_quiets()
-            && !is_capture
+            && !is_noisy
             && quiets.len()
                 >= shared_context
                     .get_lmp_lookup()
