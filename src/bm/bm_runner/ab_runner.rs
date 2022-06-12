0109038b50b4a5775053e3bdd92c931f19e3a2af
doruk <dsekercioglu2003@gmail.com>
2022-06-12 19:42:19 +0300

    TT entry aging

--- src/bm/bm_runner/ab_runner.rs
+++ src/bm/bm_runner/ab_runner.rs
@@ -478,6 +478,7 @@ impl AbRunner {
     pub fn make_move(&mut self, make_move: Move) {
         self.position.make_move(make_move);
         self.position.reset();
+        self.shared_context.t_table.age();
     }
 
     #[cfg(feature = "data")]
