f8e666290de5b315ee3d82617d5c14e342a10f62
doruk <dsekercioglu2003@gmail.com>
2022-07-08 09:37:02 +0300

    Age smoothly

--- src/bm/bm_util/t_table.rs
+++ src/bm/bm_util/t_table.rs
@@ -236,7 +236,8 @@ impl TranspositionTable {
             matches!(a.entry_type(), EntryType::Exact | EntryType::LowerBound) as u8;
         let b_extra_depth =
             matches!(b.entry_type(), EntryType::Exact | EntryType::LowerBound) as u8;
-        b.age != current_age || (a.depth + a_extra_depth) >= (b.depth + b_extra_depth) / 2
+        ((a.depth + a_extra_depth).saturating_add(current_age.wrapping_sub(b.age)))
+            >= (b.depth + b_extra_depth) / 2
     }
 
     pub fn clean(&self) {
