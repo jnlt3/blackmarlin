d147d86ff09bbe076ad9c4ab63f75e3beaec0df4
doruk <dsekercioglu2003@gmail.com>
2022-07-13 22:21:01 +0300

    Replace TT entries unconditonally if it is older than 4 plies

--- src/bm/bm_util/t_table.rs
+++ src/bm/bm_util/t_table.rs
@@ -236,8 +236,9 @@ impl TranspositionTable {
             matches!(a.entry_type(), EntryType::Exact | EntryType::LowerBound) as u8;
         let b_extra_depth =
             matches!(b.entry_type(), EntryType::Exact | EntryType::LowerBound) as u8;
-        ((a.depth + a_extra_depth).saturating_add(current_age.wrapping_sub(b.age) / 2))
-            >= ((b.depth + b_extra_depth) as u16 * 2 / 3) as u8
+        current_age.wrapping_sub(b.age) > 4
+            || ((a.depth + a_extra_depth).saturating_add(current_age.wrapping_sub(b.age) / 2))
+                >= ((b.depth + b_extra_depth) as u16 * 2 / 3) as u8
     }
 
     pub fn clean(&self) {
