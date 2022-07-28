cfcc981057c569eff3c4f8cb7c8774f4f796175a
doruk <dsekercioglu2003@gmail.com>
2022-07-28 16:49:25 +0300

    Spsa iteration 10k

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -48,7 +48,7 @@ const fn do_rev_fp(depth: u32) -> bool {
 
 #[inline]
 const fn rev_fp(depth: u32, improving: bool) -> i16 {
-    depth as i16 * 45 - improving as i16 * 39
+    depth as i16 * 42 - improving as i16 * 32
 }
 
 #[inline]
@@ -77,12 +77,12 @@ const fn iir(depth: u32) -> u32 {
 
 #[inline]
 const fn fp(depth: u32) -> i16 {
-    depth as i16 * 80
+    depth as i16 * 70
 }
 
 #[inline]
 const fn see_fp(depth: u32) -> i16 {
-    depth as i16 * 81
+    depth as i16 * 90
 }
 
 #[inline]
