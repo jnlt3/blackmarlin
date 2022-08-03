5dde372d0c333ef19c75a7c30ce324f05cf41d88
doruk <dsekercioglu2003@gmail.com>
2022-08-03 12:48:25 +0300

    Bump base LMR up by 1

--- src/bm/bm_runner/ab_runner.rs
+++ src/bm/bm_runner/ab_runner.rs
@@ -390,7 +390,7 @@ impl AbRunner {
                     if depth == 0 || mv == 0 {
                         0
                     } else {
-                        (2.0 + (depth as f32).ln() * (mv as f32).ln() / 1.75) as u32
+                        (3.0 + (depth as f32).ln() * (mv as f32).ln() / 1.75) as u32
                     }
                 })),
                 lmp_lookup: Arc::new(LookUp2d::new(|depth, improving| {
