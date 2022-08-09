ac5696a6568fd262a1d2ed7f8f4393b4be34c9b7
dsekercioglu <dsekercioglu2003@gmail.com>
2022-08-09 23:33:57 +0300

    Fix overflow issues

--- src/bm/nnue/layers.rs
+++ src/bm/nnue/layers.rs
@@ -63,37 +63,29 @@ impl<const INPUT: usize, const OUTPUT: usize> Dense<INPUT, OUTPUT> {
         out
     }
 
-    #[cfg(target_feature = "sse")]
-    unsafe fn sum_i16(x: std::arch::x86_64::__m128i) -> i32 {
-        use std::arch::x86_64::_mm_store_si128;
-        const CHUNKS: usize = 128 / 16;
+    #[cfg(target_feature = "avx2")]
+    unsafe fn sum_i16(x: std::arch::x86_64::__m256i) -> i32 {
+        use std::arch::x86_64::_mm256_store_si256;
+        const CHUNKS: usize = 256 / 16;
         let mut vector = [0_i16; CHUNKS];
-        _mm_store_si128(vector.as_mut_ptr() as *mut _, x);
+        _mm256_store_si256(vector.as_mut_ptr() as *mut _, x);
         vector.into_iter().map(|x| x as i32).sum()
     }
 
     #[cfg(target_feature = "avx2")]
     pub fn ff(&self, inputs: &[u8; INPUT]) -> [i32; OUTPUT] {
-        use std::arch::x86_64::{
-            _mm256_add_epi16, _mm256_castsi256_si128, _mm256_extracti128_si256, _mm256_load_si256,
-            _mm256_maddubs_epi16, _mm256_setzero_si256, _mm_add_epi16,
-        };
+        use std::arch::x86_64::{_mm256_load_si256, _mm256_maddubs_epi16};
         const CHUNKS: usize = 256 / 8;
         let mut out = self.bias.0;
 
         for (out, weights) in out.iter_mut().zip(&self.weights.0) {
             unsafe {
-                let mut sums = _mm256_setzero_si256();
                 for (inputs, weights) in inputs.chunks(CHUNKS).zip(weights.chunks(CHUNKS)) {
                     let inputs = _mm256_load_si256(inputs.as_ptr() as *const _);
                     let weights = _mm256_load_si256(weights.as_ptr() as *const _);
-                    let out = _mm256_maddubs_epi16(inputs, weights);
-                    sums = _mm256_add_epi16(sums, out);
+                    let mul = _mm256_maddubs_epi16(inputs, weights);
+                    *out += Self::sum_i16(mul);
                 }
-                let low = _mm256_castsi256_si128(sums);
-                let high = _mm256_extracti128_si256::<1>(sums);
-                let sum = _mm_add_epi16(low, high);
-                *out += Self::sum_i16(sum);
             }
         }
         out
