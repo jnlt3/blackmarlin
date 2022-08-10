03191852dd8200a3970439e1b58d20d9eeb1eb28
dsekercioglu <dsekercioglu2003@gmail.com>
2022-08-10 10:52:33 +0300

    Remove low history stat bonus

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -557,7 +557,7 @@ pub fn search<Search: SearchType>(
                 }
                 if score >= beta {
                     if !local_context.abort() {
-                        let amt = depth + (eval <= alpha) as u32 + (h_score < 0) as u32;
+                        let amt = depth + (eval <= alpha) as u32;
                         if !is_capture {
                             let killer_table = local_context.get_k_table();
                             killer_table[ply as usize].push(make_move);
