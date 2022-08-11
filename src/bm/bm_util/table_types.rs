ac2b620907e20b5bb5579f05b576de1281b136aa
doruk <dsekercioglu2003@gmail.com>
2022-08-11 20:21:18 +0300

    Try threat history

--- src/bm/bm_util/table_types.rs
+++ src/bm/bm_util/table_types.rs
@@ -2,6 +2,7 @@ use cozy_chess::{Piece, Square};
 
 pub type Butterfly<T> = [[T; Square::NUM]; Square::NUM];
 pub type PieceTo<T> = [[T; Square::NUM]; Piece::NUM];
+pub type Threat<T> = [T; Square::NUM + 1];
 
 pub fn new_butterfly_table<T: Copy>(default: T) -> Butterfly<T> {
     [[default; Square::NUM]; Square::NUM]
@@ -9,4 +10,8 @@ pub fn new_butterfly_table<T: Copy>(default: T) -> Butterfly<T> {
 
 pub fn new_piece_to_table<T: Copy>(default: T) -> PieceTo<T> {
     [[default; Square::NUM]; Piece::NUM]
+}
+
+pub fn new_threat_table<T: Copy>(default: T) -> Threat<T> {
+    [default; Square::NUM + 1]
 }
