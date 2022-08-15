0aa965c5834dc1e8452d1c975b8a6e4947e48fb4
dsekercioglu <dsekercioglu2003@gmail.com>
2022-08-15 20:29:19 +0300

    Double maximum prune depth

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -421,7 +421,7 @@ pub fn search<Search: SearchType>(
             && non_mate_line
             && moves_seen > 0
             && !is_capture
-            && depth <= 3
+            && depth <= 6
             && !nstm_threat.is_empty()
             && eval <= alpha;
         if do_tp && !nstm_threat.has(make_move.from) {
