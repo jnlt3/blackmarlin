7270dfa23d98844168291a4c116881d845f8b1c6
doruk <dsekercioglu2003@gmail.com>
2022-08-20 20:53:03 +0300

    Reduce quiets more

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -463,6 +463,9 @@ pub fn search<Search: SearchType>(
             if improving {
                 reduction -= 1;
             }
+            if !is_capture {
+                reduction += 1;
+            }
             if Some(make_move) == counter_move
                 || killers.into_iter().any(|killer| killer == make_move)
             {
