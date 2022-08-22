c195749ed3e2df75b3bd24c3ad78e4f05c01082c
doruk <dsekercioglu2003@gmail.com>
2022-08-22 23:21:55 +0300

    Followup move table for LMR

--- src/bm/bm_util/counter_move.rs
+++ src/bm/bm_util/counter_move.rs
@@ -1,31 +1,58 @@
-use cozy_chess::{Board, Color, Move, Piece, Square};
+use cozy_chess::{Board, Color, Move};
 
 use crate::bm::bm_runner::ab_runner::MoveData;
 
 use super::table_types::{new_piece_to_table, PieceTo};
 
 #[derive(Debug, Clone)]
-pub struct CounterMoveTable {
-    table: Box<[PieceTo<Option<Move>>; Color::NUM]>,
+pub struct MoveTable {
+    counter_move: Box<[PieceTo<Option<Move>>; Color::NUM]>,
+    followup_move: Box<[PieceTo<Option<Move>>; Color::NUM]>,
 }
 
-impl CounterMoveTable {
+impl MoveTable {
     pub fn new() -> Self {
         Self {
-            table: Box::new([new_piece_to_table(None); Color::NUM]),
+            counter_move: Box::new([new_piece_to_table(None); Color::NUM]),
+            followup_move: Box::new([new_piece_to_table(None); Color::NUM]),
         }
     }
 
-    pub fn get(&self, color: Color, piece: Piece, to: Square) -> Option<Move> {
-        self.table[color as usize][piece as usize][to as usize]
+    pub fn get_counter_move(&self, color: Color, opp_move: Option<MoveData>) -> Option<Move> {
+        opp_move
+            .map(|opp_move| {
+                self.counter_move[color as usize][opp_move.piece as usize][opp_move.to as usize]
+            })
+            .unwrap_or(None)
     }
 
-    pub fn cutoff(&mut self, board: &Board, prev_move: MoveData, cutoff_move: Move, amt: u32) {
+    pub fn get_followup_move(&self, color: Color, prev_move: Option<MoveData>) -> Option<Move> {
+        prev_move
+            .map(|prev_move| {
+                self.followup_move[color as usize][prev_move.piece as usize][prev_move.to as usize]
+            })
+            .unwrap_or(None)
+    }
+
+    pub fn cutoff(
+        &mut self,
+        board: &Board,
+        opp_move: Option<MoveData>,
+        prev_move: Option<MoveData>,
+        cutoff_move: Move,
+        amt: u32,
+    ) {
         if amt > 20 {
             return;
         }
         let color = board.side_to_move();
-        self.table[color as usize][prev_move.piece as usize][prev_move.to as usize] =
-            Some(cutoff_move);
+        opp_move.map(|opp_move| {
+            self.counter_move[color as usize][opp_move.piece as usize][opp_move.to as usize] =
+                Some(cutoff_move);
+        });
+        prev_move.map(|prev_move| {
+            self.followup_move[color as usize][prev_move.piece as usize][prev_move.to as usize] =
+                Some(cutoff_move);
+        });
     }
 }
