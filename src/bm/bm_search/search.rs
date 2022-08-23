fff5540218114f3bffefec5f89c55210ff5dcdaf
dsekercioglu <dsekercioglu2003@gmail.com>
2022-08-23 14:34:00 +0300

    If a move evades a threat and captures, extend

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -400,6 +400,10 @@ pub fn search<Search: SearchType>(
             extension = extension.max(1);
         }
 
+        if is_capture && !nstm_threat.is_empty() && !nstm_threat.has(make_move.from) {
+            extension = extension.max(1);
+        }
+
         let non_mate_line = highest_score.map_or(false, |s: Evaluation| !s.is_mate());
         /*
         In non-PV nodes If a move isn't good enough to beat alpha - a static margin
