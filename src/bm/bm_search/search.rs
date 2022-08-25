183286da9fceb3f5d91f5901f0a38c323af09f2e
dsekercioglu <dsekercioglu2003@gmail.com>
2022-08-25 12:21:17 +0300

    If TT suggests we can't beat beta, don't attempt NMP

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -223,13 +223,18 @@ pub fn search<Search: SearchType>(
         This is seen as the major threat in the current position and can be used in
         move ordering for the next ply
         */
-        if do_nmp::<Search>(
-            pos.board(),
-            depth,
-            eval.raw(),
-            beta.raw(),
-            !nstm_threat.is_empty(),
-        ) && pos.null_move()
+        let tt_supports = tt_entry.map_or(true, |entry| {
+            matches!(entry.entry_type(), EntryType::UpperBound) || entry.score() >= beta
+        });
+        if tt_supports
+            && do_nmp::<Search>(
+                pos.board(),
+                depth,
+                eval.raw(),
+                beta.raw(),
+                !nstm_threat.is_empty(),
+            )
+            && pos.null_move()
         {
             local_context.search_stack_mut()[ply as usize].move_played = None;
 
