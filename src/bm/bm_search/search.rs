47c123e46aa678410cf657696fa3a4de9f20e735
dsekercioglu <dsekercioglu2003@gmail.com>
2022-08-26 14:15:10 +0300

    Improving flag in NMP

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -223,17 +223,18 @@ pub fn search<Search: SearchType>(
         This is seen as the major threat in the current position and can be used in
         move ordering for the next ply
         */
+        let impr_eval = eval.raw() + improving as i16 * 30;
         if do_nmp::<Search>(
             pos.board(),
             depth,
-            eval.raw(),
+            impr_eval,
             beta.raw(),
             !nstm_threat.is_empty(),
         ) && pos.null_move()
         {
             local_context.search_stack_mut()[ply as usize].move_played = None;
 
-            let nmp_depth = nmp_depth(depth, eval.raw(), beta.raw());
+            let nmp_depth = nmp_depth(depth, impr_eval, beta.raw());
             let zw = beta >> Next;
             let search_score = search::<NoNm>(
                 pos,
