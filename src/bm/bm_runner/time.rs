db07770e6d6b67ed813f6db5566087d39aca77f4
dsekercioglu <dsekercioglu2003@gmail.com>
2022-09-06 14:29:22 +0300

    More aggressive time increase but reduce time more on average

--- src/bm/bm_runner/time.rs
+++ src/bm/bm_runner/time.rs
@@ -120,13 +120,13 @@ impl TimeManager {
             .powf(MOVE_CHANGE_MARGIN as f32 - move_change_depth as f32)
             .max(0.4);
 
-        let move_cnt_factor = 1.05_f32.powf(move_change_cnt as f32);
+        let move_cnt_factor = 1.1_f32.powf(move_change_cnt as f32) * 0.7;
 
         let time = time.min(self.max_duration.load(Ordering::SeqCst) as f32 * 1000.0);
         self.normal_duration
             .store((time * 0.001) as u32, Ordering::SeqCst);
         self.target_duration.store(
-            (time * 0.001 * move_change_factor * move_cnt_factor) as u32,
+            (time * 0.001 * move_change_factor * move_cnt_factor.min(1.5)) as u32,
             Ordering::SeqCst,
         );
         self.last_eval.store(current_eval, Ordering::SeqCst);
