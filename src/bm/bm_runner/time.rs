7a988c62bf159d2bbebbb555f5ddb5dbc355b63e
doruk <dsekercioglu2003@gmail.com>
2022-09-06 13:10:59 +0200

    Fix unnecessary printing and don't change base time

--- src/bm/bm_runner/time.rs
+++ src/bm/bm_runner/time.rs
@@ -120,8 +120,7 @@ impl TimeManager {
             .powf(MOVE_CHANGE_MARGIN as f32 - move_change_depth as f32)
             .max(0.4);
 
-        let move_cnt_factor = 1.05_f32.powf(move_change_cnt as f32) * 0.8;
-        println!("# {}", move_cnt_factor);
+        let move_cnt_factor = 1.05_f32.powf(move_change_cnt as f32);
 
         let time = time.min(self.max_duration.load(Ordering::SeqCst) as f32 * 1000.0);
         self.normal_duration
