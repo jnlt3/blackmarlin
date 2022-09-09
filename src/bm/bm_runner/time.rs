3a1eaf393c6e4e9c3cc64dea13984c198e005713
doruk <dsekercioglu2003@gmail.com>
2022-09-09 12:26:52 +0200

    Add the ability to quit search before depth 4

--- src/bm/bm_runner/time.rs
+++ src/bm/bm_runner/time.rs
@@ -203,9 +203,10 @@ impl TimeManager {
             self.target_duration
                 .store(move_time.as_millis() as u32, Ordering::SeqCst);
         } else {
+            let max_time = time.as_millis() as u32 / 3;
             let expected_moves = moves_to_go.unwrap_or(EXPECTED_MOVES) + 1;
             let default = if move_cnt > 1 {
-                inc.as_millis() as u32 + time.as_millis() as u32 / expected_moves
+                (inc.as_millis() as u32 + time.as_millis() as u32 / expected_moves).min(max_time)
             } else {
                 0
             };
@@ -214,7 +215,7 @@ impl TimeManager {
             self.normal_duration.store(default, Ordering::SeqCst);
             self.target_duration.store(default, Ordering::SeqCst);
             self.max_duration
-                .store(time.as_millis() as u32 / 3, Ordering::SeqCst);
+                .store(max_time, Ordering::SeqCst);
         };
     }
 
