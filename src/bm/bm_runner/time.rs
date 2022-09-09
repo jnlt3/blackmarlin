6b221dbd230e4c19b4d605b266bda8d4edb66d59
doruk <dsekercioglu2003@gmail.com>
2022-09-09 12:38:34 +0200

    Target duration can't be higher than max time

--- src/bm/bm_runner/time.rs
+++ src/bm/bm_runner/time.rs
@@ -122,11 +122,11 @@ impl TimeManager {
 
         let move_cnt_factor = 1.05_f32.powf(move_change_cnt as f32);
 
-        let time = time.min(self.max_duration.load(Ordering::SeqCst) as f32 * 1000.0);
+        let max_duration = self.max_duration.load(Ordering::SeqCst) as f32 * 1000.0;
         self.normal_duration
-            .store((time * 0.001) as u32, Ordering::SeqCst);
+            .store((time.min(max_duration) * 0.001) as u32, Ordering::SeqCst);
         self.target_duration.store(
-            (time * 0.001 * move_change_factor * move_cnt_factor) as u32,
+            (time * 0.001 * move_change_factor * move_cnt_factor).min(max_duration) as u32,
             Ordering::SeqCst,
         );
         self.last_eval.store(current_eval, Ordering::SeqCst);
