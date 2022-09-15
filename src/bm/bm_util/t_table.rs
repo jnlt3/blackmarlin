8ba819166fef54380ca5146a27178ee3185b46fc
dsekercioglu <dsekercioglu2003@gmail.com>
2022-09-15 12:15:24 +0200

    Use a static margin for TT replacement

--- src/bm/bm_util/t_table.rs
+++ src/bm/bm_util/t_table.rs
@@ -236,8 +236,8 @@ impl TranspositionTable {
             matches!(a.entry_type(), EntryType::Exact | EntryType::LowerBound) as u8;
         let b_extra_depth =
             matches!(b.entry_type(), EntryType::Exact | EntryType::LowerBound) as u8;
-        ((a.depth + a_extra_depth).saturating_add(current_age.wrapping_sub(b.age) / 2))
-            >= (b.depth + b_extra_depth) / 2
+        ((a.depth + a_extra_depth).saturating_add(current_age.wrapping_sub(b.age) / 2)) + 3
+            >= (b.depth + b_extra_depth)
     }
 
     pub fn clean(&self) {
