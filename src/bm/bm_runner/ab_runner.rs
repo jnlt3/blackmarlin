3fcf100efa7c39207cb92e33ae2498742ad73bb5
dsekercioglu <dsekercioglu2003@gmail.com>
2022-09-20 16:07:27 +0200

    Increase aspiration window size slightly

--- src/bm/bm_runner/ab_runner.rs
+++ src/bm/bm_runner/ab_runner.rs
@@ -451,7 +451,7 @@ impl AbRunner {
                 start: Instant::now(),
             },
             local_context: LocalContext {
-                window: Window::new(12, 1, 4, 5),
+                window: Window::new(16, 1, 4, 5),
                 tt_hits: 0,
                 tt_misses: 0,
                 eval: position.get_eval(Color::White, Evaluation::new(0)),
