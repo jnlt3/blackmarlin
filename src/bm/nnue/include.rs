a4ed52c81a6b5962d9a4d35ff1bfad52f64eec58
dsekercioglu <dsekercioglu2003@gmail.com>
2022-10-01 12:51:22 +0200

    Don't rely on compiler optimizations to prevent stack overflow

--- src/bm/nnue/include.rs
+++ src/bm/nnue/include.rs
@@ -1,20 +1,23 @@
+use std::alloc::Layout;
+
 use super::layers::Align;
 
-pub fn sparse_from_bytes_i16<
-    T: From<i16> + Copy + Default,
-    const INPUT: usize,
-    const OUTPUT: usize,
->(
+pub fn sparse_from_bytes_i16<const INPUT: usize, const OUTPUT: usize>(
     bytes: &[u8],
-) -> Box<Align<[[T; OUTPUT]; INPUT]>> {
+) -> Box<Align<[[i16; OUTPUT]; INPUT]>> {
     let mut weights = vec![];
     for bytes in bytes.chunks(2).take(INPUT * OUTPUT) {
         weights.push(i16::from_le_bytes([bytes[0], bytes[1]]))
     }
-    let mut dense = Box::new(Align([[T::default(); OUTPUT]; INPUT]));
+
+    let mut dense = unsafe {
+        let mem = std::alloc::alloc_zeroed(Layout::new::<Align<[[i16; OUTPUT]; INPUT]>>());
+        Box::<Align<[[i16; OUTPUT]; INPUT]>>::from_raw(mem as *mut _)
+    };
+
     for (i, weights) in weights.chunks(OUTPUT).enumerate() {
         for (j, &weight) in weights.iter().enumerate() {
-            dense.0[i][j] = T::from(weight);
+            dense.0[i][j] = weight;
         }
     }
     dense
