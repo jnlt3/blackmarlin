7ef9c5df813c3b0153432f26d4ddaf305c3ae04c
dsekercioglu <dsekercioglu2003@gmail.com>
2022-10-05 11:42:02 +0200

    Initialize history to be lower for more pruning early on

--- src/bm/bm_util/table_types.rs
+++ src/bm/bm_util/table_types.rs
@@ -3,10 +3,12 @@ use cozy_chess::{Piece, Square};
 pub type Butterfly<T> = [[T; Square::NUM]; Square::NUM];
 pub type PieceTo<T> = [[T; Square::NUM]; Piece::NUM];
 
+#[inline(never)]
 pub fn new_butterfly_table<T: Copy>(default: T) -> Butterfly<T> {
     [[default; Square::NUM]; Square::NUM]
 }
 
+#[inline(never)]
 pub fn new_piece_to_table<T: Copy>(default: T) -> PieceTo<T> {
     [[default; Square::NUM]; Piece::NUM]
 }
