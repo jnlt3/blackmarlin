ad9796d43983b84c6889d416d9543b7f51522d75
dsekercioglu <dsekercioglu2003@gmail.com>
2022-10-12 16:47:48 +0200

    Quiet moves have a much lower pruning margin

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -88,8 +88,8 @@ const fn fp(depth: u32) -> i16 {
     depth as i16 * 62
 }
 
-const fn see_fp(depth: u32) -> i16 {
-    depth as i16 * 81
+const fn see_fp(depth: u32, quiet: bool) -> i16 {
+    depth as i16 * if quiet { 20 } else { 81 }
 }
 
 const fn hp(depth: u32) -> i32 {
@@ -433,7 +433,7 @@ pub fn search<Search: SearchType>(
         */
         let do_see_prune = !Search::PV && non_mate_line && moves_seen > 0 && depth <= 7;
 
-        if do_see_prune && !compare_see(pos.board(), make_move, -see_fp(depth)) {
+        if do_see_prune && !compare_see(pos.board(), make_move, -see_fp(depth, !is_capture)) {
             continue;
         }
 
