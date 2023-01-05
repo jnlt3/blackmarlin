6db7aa4b8dfd0f6f7d2025d5a2666b8585ca91c5
dsekercioglu <dsekercioglu2003@gmail.com>
2023-01-05 18:16:29 +0100

    Spend more time if the position is non decisive

--- src/bm/bm_runner/time.rs
+++ src/bm/bm_runner/time.rs
@@ -122,11 +122,14 @@ impl TimeManager {
 
         let move_cnt_factor = 1.05_f32.powf(move_change_cnt as f32);
 
+        let winnability_factor = 1.0 + (200 - current_eval.abs()) as f32 / 1000.0;
+
         let max_duration = self.max_duration.load(Ordering::SeqCst) as f32 * 1000.0;
         self.normal_duration
             .store((time.min(max_duration) * 0.001) as u32, Ordering::SeqCst);
         self.target_duration.store(
-            (time * 0.001 * move_change_factor * move_cnt_factor).min(max_duration * 0.001) as u32,
+            (time * 0.001 * move_change_factor * move_cnt_factor * winnability_factor)
+                .min(max_duration * 0.001) as u32,
             Ordering::SeqCst,
         );
         self.last_eval.store(current_eval, Ordering::SeqCst);
