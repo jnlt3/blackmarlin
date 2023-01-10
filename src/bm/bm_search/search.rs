52c24951afe35a731e0cd03b8527f701b516981d
dsekercioglu <dsekercioglu2003@gmail.com>
2023-01-10 11:08:38 +0100

    Increase futility pruning depth

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -396,7 +396,7 @@ pub fn search<Search: SearchType>(
         In non-PV nodes If a move isn't good enough to beat alpha - a static margin
         we assume it's safe to prune this move
         */
-        let do_fp = !Search::PV && non_mate_line && moves_seen > 0 && !is_capture && depth <= 5;
+        let do_fp = !Search::PV && non_mate_line && moves_seen > 0 && !is_capture && depth <= 8;
 
         if do_fp && eval + fp(depth) <= alpha {
             move_gen.skip_quiets();
