9369a9555eec20db772f237d5e6d09960aa1c29b
dsekercioglu <dsekercioglu2003@gmail.com>
2023-08-12 02:09:37 +0300

    Half margin

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -683,7 +683,7 @@ pub fn q_search(
         if stand_pat + 200 <= alpha && see <= 0 {
             continue;
         }
-        if see + 400 < best_see {
+        if see + 200 < best_see {
             continue;
         }
         pos.make_move(make_move);
