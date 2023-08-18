6c7ce53ace1f8396377e4280ab6d306397762f87
dsekercioglu <dsekercioglu2003@gmail.com>
2023-08-18 10:53:11 +0300

    Less aggressive TT replacement scheme

--- src/bm/bm_util/t_table.rs
+++ src/bm/bm_util/t_table.rs
@@ -236,8 +236,8 @@ impl TranspositionTable {
             matches!(a.entry_type(), EntryType::Exact | EntryType::LowerBound) as u8;
         let b_extra_depth =
             matches!(b.entry_type(), EntryType::Exact | EntryType::LowerBound) as u8;
-        ((a.depth + a_extra_depth).saturating_add(current_age.wrapping_sub(b.age) / 2))
-            >= (b.depth + b_extra_depth) / 2
+        ((a.depth + a_extra_depth).saturating_add(current_age.wrapping_sub(b.age) / 2)) as u16
+            >= (b.depth + b_extra_depth) as u16 * 2 / 3
     }
 
     pub fn clean(&self) {
