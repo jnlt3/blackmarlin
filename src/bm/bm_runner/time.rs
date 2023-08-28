b71692ce6d08d7ff24f10cb2f85320d98c59060b
dsekercioglu <dsekercioglu2003@gmail.com>
2023-08-28 18:50:56 +0300

    Tweak

--- src/bm/bm_runner/time.rs
+++ src/bm/bm_runner/time.rs
@@ -100,7 +100,7 @@ impl TimeManager {
         self.move_stability.store(move_stability, Ordering::Relaxed);
 
         let move_stability_factor = (50 - move_stability) as f32 / 40.0;
-        let eval_stability_factor = (prev_eval - eval).clamp(10, 30) as f32 / 20.0;
+        let eval_stability_factor = (prev_eval - eval).clamp(10, 30) as f32 / 15.0;
 
         let base_duration = self.base_duration.load(Ordering::Relaxed);
         let target_duration = base_duration as f32 * move_stability_factor * eval_stability_factor;
