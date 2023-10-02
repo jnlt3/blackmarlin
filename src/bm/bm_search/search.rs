9c5ae12a47e7cb3292840e6914bce8303de2c014
dsekercioglu <dsekercioglu2003@gmail.com>
2023-10-02 15:53:30 +0200

    If eval is significantly lower than alpha, add more history bonus

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -547,7 +547,10 @@ pub fn search<Search: SearchType>(
                 }
                 if score >= beta {
                     if !thread.abort {
-                        let amt = depth + (eval <= alpha) as u32 + (score - 50 > beta) as u32;
+                        let amt = depth
+                            + (eval <= alpha) as u32
+                            + (eval + 50 <= alpha) as u32
+                            + (score - 50 > beta) as u32;
                         if !is_capture {
                             thread.killer_moves[ply as usize].push(make_move);
                         }
