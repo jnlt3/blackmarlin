91b3dc18ef170d64fc401cd9887885d22704b049
jnlt3 <dsekercioglu2003@gmail.com>
2024-01-17 16:54:40 +0000

    Add a square factor to history

--- src/bm/bm_util/history.rs
+++ src/bm/bm_util/history.rs
@@ -8,7 +8,7 @@ use super::table_types::{new_butterfly_table, new_piece_to_table, Butterfly, Pie
 pub const MAX_HIST: i16 = 512;
 
 fn hist_stat(amt: i16) -> i16 {
-    (amt * 16).min(MAX_HIST)
+    (amt * amt + amt * 16).min(MAX_HIST)
 }
 
 fn bonus(hist: &mut i16, amt: i16) {
