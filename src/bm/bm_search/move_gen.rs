28f32f2df14f7afaac4cc573900ae033e8423f2b
jnlt3 <dsekercioglu2003@gmail.com>
2024-01-18 22:17:58 +0000

    Remove MVV

--- src/bm/bm_search/move_gen.rs
+++ src/bm/bm_search/move_gen.rs
@@ -132,7 +132,7 @@ impl OrderedMoveGen {
                     if let Some(index) = self.killers.index_of(mv) {
                         self.killers.remove(index);
                     }
-                    let score = hist.get_capture(pos, mv) + move_value(pos.board(), mv) * 32;
+                    let score = hist.get_capture(pos, mv);
                     self.captures.push(Capture::new(mv, score))
                 }
             }
