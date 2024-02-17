e992718436c8b189da5fac119347a387d1dc0204
jnlt3 <dsekercioglu2003@gmail.com>
2024-02-17 13:34:46 +0000

    Don't do history pruning for good captures

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -408,7 +408,12 @@ pub fn search<Search: SearchType>(
         In low depth, non-PV nodes, we assume it's safe to prune a move
         if it has very low history
         */
-        let do_hp = !Search::PV && non_mate_line && moves_seen > 0 && depth <= 6 && eval <= alpha;
+        let do_hp = !Search::PV
+            && non_mate_line
+            && moves_seen > 0
+            && depth <= 6
+            && move_gen.phase() > Phase::GoodCaptures
+            && eval <= alpha;
 
         if do_hp && (h_score as i32) < hp(depth) {
             continue;
