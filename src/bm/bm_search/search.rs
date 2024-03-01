74a27ee26dd722b62248dba74f330a4c2c318bbd
jnlt3 <dsekercioglu2003@gmail.com>
2024-03-01 18:48:12 +0000

    Don't accept upperbound entries

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -223,7 +223,9 @@ pub fn search<Search: SearchType>(
         This is seen as the major threat in the current position and can be used in
         move ordering for the next ply
         */
-        let tt_do_nmp = tt_entry.map_or(true, |entry| !entry.nmp_fail || entry.depth < depth);
+        let tt_do_nmp = tt_entry.map_or(true, |entry| {
+            !(entry.nmp_fail && entry.depth >= depth && entry.bounds != Bounds::UpperBound)
+        });
         if tt_do_nmp
             && do_nmp::<Search>(
                 pos.board(),
