7e64091faa43d123a14be357ebeecf461ea761cc
jnlt3 <dsekercioglu2003@gmail.com>
2024-03-02 11:01:25 +0000

    history bonus for quiet moves

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -555,7 +555,10 @@ pub fn search<Search: SearchType>(
                 }
                 if score >= beta {
                     if !thread.abort {
-                        let amt = depth + (eval <= alpha) as u32 + (score - 50 > beta) as u32;
+                        let amt = depth
+                            + (eval <= alpha) as u32
+                            + (score - 50 > beta) as u32
+                            + !is_capture as u32;
                         if !is_capture {
                             thread.killer_moves[ply as usize].push(make_move);
                         }
