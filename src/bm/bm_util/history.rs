9366a2700c8232b804634269d3bc0cf4c1b64793
jnlt3 <dsekercioglu2003@gmail.com>
2024-03-07 18:24:37 +0000

    Quiet passer history

--- src/bm/bm_util/history.rs
+++ src/bm/bm_util/history.rs
@@ -46,7 +46,7 @@ impl HistoryIndices {
 #[derive(Debug, Clone)]
 pub struct History {
     quiet: Box<[[Butterfly<i16>; 2]; Color::NUM]>,
-    capture: Box<[[Butterfly<i16>; 2]; Color::NUM]>,
+    capture: Box<[Butterfly<i16>; Color::NUM]>,
     counter_move: Box<[PieceTo<PieceTo<i16>>; Color::NUM]>,
     followup_move: Box<[PieceTo<PieceTo<i16>>; Color::NUM]>,
 }
@@ -55,7 +55,7 @@ impl History {
     pub fn new() -> Self {
         Self {
             quiet: Box::new([[new_butterfly_table(0); 2]; Color::NUM]),
-            capture: Box::new([[new_butterfly_table(0); 2]; Color::NUM]),
+            capture: Box::new([new_butterfly_table(0); Color::NUM]),
             counter_move: Box::new([new_piece_to_table(new_piece_to_table(0)); Color::NUM]),
             followup_move: Box::new([new_piece_to_table(new_piece_to_table(0)); Color::NUM]),
         }
@@ -78,15 +78,12 @@ impl History {
     /// Returns capture history value for the given move
     pub fn get_capture(&self, pos: &Position, make_move: Move) -> i16 {
         let stm = pos.board().side_to_move();
-        let passer = pos.is_passer(make_move.from, stm);
-        self.capture[stm as usize][passer as usize][make_move.from as usize][make_move.to as usize]
+        self.capture[stm as usize][make_move.from as usize][make_move.to as usize]
     }
 
     fn get_capture_mut(&mut self, pos: &Position, make_move: Move) -> &mut i16 {
         let stm = pos.board().side_to_move();
-        let passer = pos.is_passer(make_move.from, stm);
-        &mut self.capture[stm as usize][passer as usize][make_move.from as usize]
-            [make_move.to as usize]
+        &mut self.capture[stm as usize][make_move.from as usize][make_move.to as usize]
     }
 
     /// Returns None if a previous move isn't available
