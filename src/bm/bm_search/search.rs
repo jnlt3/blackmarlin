5e4618cfbba87ad83f9622dfe16475ed58d613b7
jnlt3 <dsekercioglu2003@gmail.com>
2024-03-11 19:01:02 +0000

    Negative extend more on non pv nodes

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -371,7 +371,10 @@ pub fn search<Search: SearchType>(
                     */
                     return s_beta;
                 } else if multi_cut && entry.score >= beta {
-                    extension = -1;
+                    extension = match Search::PV {
+                        true => -2,
+                        false => -3,
+                    }
                 }
             }
         }
