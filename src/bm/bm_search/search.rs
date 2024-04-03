d692229da09a6eec51dc769eb276567732ea97e8
jnlt3 <dsekercioglu2003@gmail.com>
2024-04-03 12:40:11 +0100

    Use low depth extension formula for triple extensions

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -365,7 +365,7 @@ pub fn search<Search: SearchType>(
                     extension = 1;
                     if !Search::PV && multi_cut && s_score + 2 < s_beta {
                         extension += 1;
-                        if !is_capture && s_score + 180 < s_beta {
+                        if !is_capture && s_score + 180 < s_beta && eval < s_beta {
                             extension += 1;
                         }
                     }
