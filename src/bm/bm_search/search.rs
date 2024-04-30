4cc87125541b844f0c34655f26c6b10387c17b53
jnlt3 <dsekercioglu2003@gmail.com>
2024-04-30 13:19:32 +0100

    remove eval <= alpha competition

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -434,12 +434,7 @@ pub fn search<Search: SearchType>(
         In low depth, non-PV nodes, we assume it's safe to prune a move
         if it has very low history
         */
-        let do_hp = !Search::PV
-            && non_mate_line
-            && moves_seen > 0
-            && !good_capture
-            && depth <= 6
-            && eval <= alpha;
+        let do_hp = !Search::PV && non_mate_line && moves_seen > 0 && !good_capture && depth <= 6;
 
         if do_hp && (h_score as i32) < hp(depth) {
             continue;
