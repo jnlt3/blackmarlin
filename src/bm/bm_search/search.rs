1d24fa3ec70288549dc308711c2a1ee15b977f70
jnlt3 <dsekercioglu2003@gmail.com>
2024-05-01 17:01:19 +0100

    Don't enforce eval <= alpha for bad captures

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -433,7 +433,11 @@ pub fn search<Search: SearchType>(
         In low depth, non-PV nodes, we assume it's safe to prune a move
         if it has very low history
         */
-        let do_hp = !Search::PV && non_mate_line && moves_seen > 0 && depth <= 6 && eval <= alpha;
+        let do_hp = !Search::PV
+            && non_mate_line
+            && moves_seen > 0
+            && depth <= 6
+            && (eval <= alpha || move_gen.phase() > Phase::Quiets);
 
         if do_hp && (h_score as i32) < hp(depth) {
             continue;
