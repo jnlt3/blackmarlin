50cce7739b79b588c99c0b24a8f4ad3628fbcd15
jnlt3 <dsekercioglu2003@gmail.com>
2024-05-04 18:55:34 +0100

    Reduce margin on ld-dse-3

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -376,6 +376,9 @@ pub fn search<Search: SearchType>(
                             extension += 1;
                         }
                     }
+                    if !multi_cut && eval + 200 <= alpha {
+                        extension += 1;
+                    }
                     thread.history.update_history(
                         pos,
                         &hist_indices,
