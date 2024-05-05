1d6766b780d3b09362f093c2aab98d7a721f7200
jnlt3 <dsekercioglu2003@gmail.com>
2024-05-05 11:30:00 +0100

    Reduce pruning depth if move is a good capture

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -436,10 +436,14 @@ pub fn search<Search: SearchType>(
         In low depth, non-PV nodes, we assume it's safe to prune a move
         if it has very low history
         */
+        let hp_depth = match good_capture {
+            true => 4,
+            false => 6,
+        };
         let do_hp = !Search::PV
             && non_mate_line
             && moves_seen > 0
-            && depth <= 6
+            && depth <= hp_depth
             && (!good_capture || eval <= alpha);
 
         if do_hp && (h_score as i32) < hp(depth) {
