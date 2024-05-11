fba69f9efc4e3a9e73513fbf3face3150d5be995
jnlt3 <dsekercioglu2003@gmail.com>
2024-05-11 16:40:37 +0100

    Less LMR if a move gives check and causes more threats

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -507,6 +507,9 @@ pub fn search<Search: SearchType>(
             }
             if new_stm_threat.len() > stm_threats.len() {
                 reduction -= 1;
+                if gives_check {
+                    reduction -= 1;
+                }
             }
             reduction = reduction.min(depth as i16 - 2).max(0);
         }
