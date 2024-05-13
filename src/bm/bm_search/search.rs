7b71b05794be8afc3d5209946d50f00df42eba6c
jnlt3 <dsekercioglu2003@gmail.com>
2024-05-13 13:49:53 +0100

    Double extend in PV nodes

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -380,6 +380,9 @@ pub fn search<Search: SearchType>(
                             extension += 1;
                         }
                     }
+                    if Search::PV && multi_cut && s_score + 180 < s_beta {
+                        extension += 1;
+                    }
                     if !Search::PV && !multi_cut && eval + 100 <= alpha {
                         extension += 1;
                     }
