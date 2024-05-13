0a3b18a61c341c8c5fbb40969c81d063270383e4
jnlt3 <dsekercioglu2003@gmail.com>
2024-05-13 14:43:02 +0100

    Reduce extension margin

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -380,7 +380,7 @@ pub fn search<Search: SearchType>(
                             extension += 1;
                         }
                     }
-                    if Search::PV && multi_cut && s_score + 180 < s_beta {
+                    if Search::PV && multi_cut && s_score + 120 < s_beta {
                         extension += 1;
                     }
                     if !Search::PV && !multi_cut && eval + 100 <= alpha {
