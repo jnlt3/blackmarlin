a441fe9eecb4b9879b91d4dfc22a7ea9a5bdbfb4
jnlt3 <dsekercioglu2003@gmail.com>
2024-05-24 15:56:22 +0100

    More LMR on threatened pieces

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -508,6 +508,9 @@ pub fn search<Search: SearchType>(
             if new_stm_threat.len() > stm_threats.len() {
                 reduction -= 1;
             }
+            if nstm_threats.has(make_move.from) {
+                reduction -= 1;
+            }
             reduction = reduction.min(depth as i16 - 2).max(0);
         }
 
