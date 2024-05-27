ed812e9835146430c72586edeba98f7ae38c1961
jnlt3 <dsekercioglu2003@gmail.com>
2024-05-27 01:08:00 +0100

    Reduce PV double extension margin

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -377,7 +377,7 @@ pub fn search<Search: SearchType>(
                             extension += 1;
                         }
                     }
-                    if Search::PV && multi_cut && s_score + 120 < s_beta {
+                    if Search::PV && multi_cut && s_score + 80 < s_beta {
                         extension += 1;
                     }
                     if !Search::PV && !multi_cut && eval + 100 <= alpha {
