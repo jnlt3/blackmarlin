1b9188df8fcb567e6a6e6a58ae6779f83c2ff1eb
jnlt3 <dsekercioglu2003@gmail.com>
2024-05-27 16:36:26 +0100

    Prefer low pawn count when winning

--- src/bm/bm_util/position.rs
+++ src/bm/bm_util/position.rs
@@ -198,8 +198,9 @@ impl Position {
     /// - Value may vary depending on position and root evaluation
     /// - Avoid storing, instead recalculate for a given position
     pub fn aggression(&self, stm: Color, root_eval: Evaluation) -> i16 {
-        let piece_cnt = self.board().occupied().len() - self.board().pieces(Piece::Pawn).len();
-        let scale = 2 * piece_cnt as i16;
+        let pawn_cnt = self.board().pieces(Piece::Pawn).len();
+        let piece_cnt = self.board().occupied().len() - pawn_cnt;
+        let scale = 2 * piece_cnt as i16 - pawn_cnt as i16 + 8;
 
         let clamped_eval = root_eval.raw().clamp(-200, 200);
         (match self.board().side_to_move() == stm {
