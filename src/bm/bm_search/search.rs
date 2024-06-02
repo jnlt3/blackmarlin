2af7292803082c4177ca7359359a28717249d1a1
jnlt3 <dsekercioglu2003@gmail.com>
2024-06-02 18:45:06 +0100

    Double extend all low depth in Non PV

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -371,16 +371,13 @@ pub fn search<Search: SearchType>(
                 thread.ss[ply as usize].skip_move = None;
                 if s_score < s_beta {
                     extension = 1;
-                    if !Search::PV && multi_cut && s_score < s_beta {
+                    if !Search::PV && s_score < s_beta {
                         extension += 1;
-                        if !is_capture && s_score + 197 < s_beta {
+                        if !is_capture && multi_cut && s_score + 197 < s_beta {
                             extension += 1;
                         }
                     }
                     if Search::PV && multi_cut && s_score + 120 < s_beta {
-                        extension += 1;
-                    }
-                    if !Search::PV && !multi_cut && eval + 100 <= alpha {
                         extension += 1;
                     }
                     thread.history.update_history(
