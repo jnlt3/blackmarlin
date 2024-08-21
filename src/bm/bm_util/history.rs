437b7743b59cac6a10a4afdf31f39e59201516cc
jnlt3 <dsekercioglu2003@gmail.com>
2024-08-22 02:40:34 +0300

    More aggressive decay

--- src/bm/bm_util/history.rs
+++ src/bm/bm_util/history.rs
@@ -285,7 +285,7 @@ impl History {
         let hash = pos.pawn_hash();
         let prev_value = self.pawn_corr[stm as usize][hash as usize];
         let weight = (depth * 8).min(128) as i32;
-        let new_value = prev_value + eval_diff as i32 * weight - prev_value / CORR_HIST_GRAIN;
+        let new_value = prev_value + eval_diff as i32 * weight - prev_value / 32;
         self.pawn_corr[stm as usize][hash as usize] = new_value.clamp(
             -MAX_CORRECT * CORR_HIST_GRAIN,
             MAX_CORRECT * CORR_HIST_GRAIN,
