077c78de9c6611931aca3390332e8e1b0c954aab
jnlt3 <dsekercioglu2003@gmail.com>
2024-08-23 12:26:00 +0300

    Reduce pawn hist weight

--- src/bm/bm_search/move_gen.rs
+++ src/bm/bm_search/move_gen.rs
@@ -197,11 +197,11 @@ impl OrderedMoveGen {
                             let followup_move_hist_2 = hist
                                 .get_followup_move_2(pos, hist_indices, mv)
                                 .unwrap_or_default();
-                            quiet_hist
+                            quiet_hist * 2
                                 + pawn_hist
-                                + counter_move_hist
-                                + followup_move_hist
-                                + followup_move_hist_2
+                                + counter_move_hist * 2
+                                + followup_move_hist * 2
+                                + followup_move_hist_2 * 2
                         }
                     };
                     self.quiets.push(ScoredMove::new(mv, score));
