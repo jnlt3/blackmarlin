2e512fece61e4f13c0ae865f8c03e8d834f22eca
jnlt3 <dsekercioglu2003@gmail.com>
2024-08-30 14:10:26 +0300

    Cutnode hist update

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -599,7 +599,10 @@ pub fn search<Search: SearchType>(
                 }
                 if score >= beta {
                     if !thread.abort {
-                        let amt = depth + (eval <= alpha) as u32 + (score - 50 > beta) as u32;
+                        let amt = depth
+                            + (eval <= alpha) as u32
+                            + (score - 50 > beta) as u32
+                            + cut_node as u32;
                         if !is_capture {
                             thread.killer_moves[ply as usize].push(make_move);
                         }
